a94c571bf2ad125544e8570c1dbe131c103c44b4
alexwebdev05 <alexwebdev05@gmail.com>
2025-11-27 15:13:20 +0100

    .gitignore

--- target/debug/deps/MemoryCacheSystem-af1077a106b3e134.d
+++
@@ -1,5 +0,0 @@
-c:\Programacion\MemoryCacheSystem\target\debug\deps\MemoryCacheSystem-af1077a106b3e134.d: src\main.rs
-
-c:\Programacion\MemoryCacheSystem\target\debug\deps\libMemoryCacheSystem-af1077a106b3e134.rmeta: src\main.rs
-
-src\main.rs:
--- target/debug/deps/MemoryCacheSystem-fc03a6ab5345b751.d
+++
@@ -1,5 +0,0 @@
-c:\Programacion\MemoryCacheSystem\target\debug\deps\MemoryCacheSystem-fc03a6ab5345b751.d: src\main.rs
-
-c:\Programacion\MemoryCacheSystem\target\debug\deps\libMemoryCacheSystem-fc03a6ab5345b751.rmeta: src\main.rs
-
-src\main.rs:
