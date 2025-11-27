547db736f5eb94634ad415777396bc85c3d0b0cf
alexwebdev05 <alexwebdev05@proton.me>
2025-11-27 13:05:56 +0100

    Project: Repository initialization

---
+++ target/debug/deps/MemoryCacheSystem-af1077a106b3e134.d
@@ -0,0 +1,5 @@
+c:\Programacion\MemoryCacheSystem\target\debug\deps\MemoryCacheSystem-af1077a106b3e134.d: src\main.rs
+
+c:\Programacion\MemoryCacheSystem\target\debug\deps\libMemoryCacheSystem-af1077a106b3e134.rmeta: src\main.rs
+
+src\main.rs:
